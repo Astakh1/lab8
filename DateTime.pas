3a98f1b1005e949cdc376805b82f7ef61d96e352
Lender <ivan2008.astahov@gmail.com>
2024-10-05 09:55:48 +0300

    Add is leap year programm

---
+++ DateTime.pas
@@ -0,0 +1,13 @@
+﻿begin
+  var year := ReadInteger('Введите год для проверки:');
+  Assert(year > 0);
+  var isleap := False;
+  if year.Divs(4) then
+    if year.Divs(100) then
+      if year.Divs(400) then
+        isleap := True
+      else
+    else
+      isleap := True;
+  Println($'Год является високосным: {isleap}');  
+end.
